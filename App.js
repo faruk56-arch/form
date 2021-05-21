43acd1743dc75189bc1cd6878d35d50b0c0aff8f
faruk <faruk373@gmail.com>
2021-05-21 17:03:24 +0200

    commit-form

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+import { StyleSheet, Button, View, TextInput, number, text } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const regex = new RegExp("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+  function signIn() {
+    if ((regex.test(email) === true) && (password.length >= 6)) {
+      alert("email et password correct")
+    } else {
+      alert("email et password wrong")
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        onChange={(e) => { setEmail(e.target.value) }}
+        value={text}
+        placeholder="email"
+      />
+      <TextInput
+        style={styles.input}
+        onChange={(e) => { setPassword(e.target.value) }}
+        value={number}
+        placeholder="Password"
+        keyboardType="numeric"
+      />
+      <Button
+        title="Sign in"
+        onPress={signIn}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +45,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 10,
+    borderWidth: 1
+  }
 });
+
